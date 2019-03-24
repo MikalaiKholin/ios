2f0f378fc88e9e296b5a5aca8efd2b94abe54540
MikalaiKholin <kholin.mikalai@gmail.com>
2019-03-24 06:36:57 -0700

    1

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/1/SummArray.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/1/SummArray.m
@@ -4,7 +4,16 @@
 
 // Complete the summArray function below.
 - (NSNumber *)summArray:(NSArray *)array {
-    return @(0);
+    int sumint=0;
+    for (NSString *num in array){
+        sumint += [num integerValue];
+    }
+    
+    NSNumber *sum = [NSNumber numberWithInt:sumint];
+
+     //NSNumber *sum = [array valueForKeyPath: @"@sum.self"]; lifehack
+    
+    return sum;
 }
 
 @end
