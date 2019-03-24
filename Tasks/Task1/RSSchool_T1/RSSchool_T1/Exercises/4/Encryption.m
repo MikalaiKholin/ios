42db7c95b58a43136d2a2c7056b116deaf041a8e
MikalaiKholin <kholin.mikalai@gmail.com>
2019-03-24 14:17:34 -0700

    3

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/4/Encryption.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/4/Encryption.m
@@ -4,7 +4,37 @@
 
 // Complete the encryption function below.
 - (NSString *)encryption:(NSString *)string {
-    return @"";
+    
+    NSString *stringWithoutSpaces = [string stringByReplacingOccurrencesOfString:@" "
+                                                                      withString:@""];
+    int row = 0;
+    int column = 0;
+    
+    row = (int)sqrt(stringWithoutSpaces.length);
+    
+    if (row*row>=stringWithoutSpaces.length){
+        column = row;
+    }
+    else{
+        column = row + 1;
+    }
+    
+    NSMutableString *mEncryptedString = [[NSMutableString alloc] initWithString:@""];
+    
+    for (int i=0; i<column; i++){
+        for (int j=0; j<=row; j++){
+            if (j*column+i<stringWithoutSpaces.length) {
+                [mEncryptedString appendFormat:@"%C", [stringWithoutSpaces characterAtIndex:j*column+i]];
+            }
+        }
+        if (i<column-1){
+            [mEncryptedString appendFormat:@" "];
+        }
+    }
+    
+    NSString *encryptedString = [NSString stringWithString:mEncryptedString];
+    
+    return encryptedString;
 }
 
 @end
--- Tasks/Task1/RSSchool_T1/RSSchool_T1/main.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/main.m
@@ -1,8 +1,10 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
+#import "Exercises/4/Encryption.h"
 
 int main(int argc, char * argv[]) {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    @autoreleasepool {   
+            return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
+    
 }
