0db0e7f3892f77b295a8659ca174b3552d0ceeab
MikalaiKholin <kholin.mikalai@gmail.com>
2019-03-24 07:34:42 -0700

    2

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/1/SummArray.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/1/SummArray.m
@@ -4,12 +4,12 @@
 
 // Complete the summArray function below.
 - (NSNumber *)summArray:(NSArray *)array {
-    int sumint=0;
+    int sumInt=0;
     for (NSString *num in array){
-        sumint += [num integerValue];
+        sumInt += [num integerValue];
     }
     
-    NSNumber *sum = [NSNumber numberWithInt:sumint];
+    NSNumber *sum = [NSNumber numberWithInt:sumInt];
 
      //NSNumber *sum = [array valueForKeyPath: @"@sum.self"]; lifehack
     
--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/2/Diagonal.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/2/Diagonal.m
@@ -4,7 +4,19 @@
 
 // Complete the diagonalDifference function below.
 - (NSNumber *) diagonalDifference:(NSArray *)array {
-    return 0;
+    
+    int diagonalDifferenceInt = 0;
+    
+    for (int i=0; i<array.count; i++) {
+        
+        NSString *stringLine = array[i];
+        NSArray *arrayLine = [stringLine componentsSeparatedByString:@" "];
+        diagonalDifferenceInt += [arrayLine[i] integerValue] - [arrayLine[array.count - i - 1] integerValue];
+    }
+    
+    NSNumber *diagonalDifference= [NSNumber numberWithInt:ABS(diagonalDifferenceInt)];
+    
+    return diagonalDifference;
 }
 
 @end
