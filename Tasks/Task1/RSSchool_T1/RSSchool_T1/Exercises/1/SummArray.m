95fb8dae59932a96126f1511764a3e11887a1e01
MikalaiKholin <kholin.mikalai@gmail.com>
2019-03-24 10:22:21 -0700

    3

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/1/SummArray.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/1/SummArray.m
@@ -4,6 +4,7 @@
 
 // Complete the summArray function below.
 - (NSNumber *)summArray:(NSArray *)array {
+    
     int sumInt=0;
     for (NSString *num in array){
         sumInt += [num integerValue];
--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/3/Pangrams.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/3/Pangrams.m
@@ -4,7 +4,21 @@
 
 // Complete the pangrams function below.
 - (BOOL)pangrams:(NSString *)string {
-    return NO;
+    
+    NSString *lowerCase = [string lowercaseString];
+    
+    NSArray *alphabet = @[@"a",@"b",@"c",@"d",@"e",@"f",@"g",@"h",@"i",@"j",@"k",@"l",
+                          @"m",@"n",@"o",@"p",@"q",@"r",@"s",@"t",@"u",@"v",@"w",@"x",@"y",@"z"];
+    
+    for (int i=0; i<alphabet.count; i++) {
+        
+        if (![lowerCase containsString:alphabet[i]]) {
+            return NO;
+        }
+        
+    }
+    
+    return YES;
 }
 
 @end
