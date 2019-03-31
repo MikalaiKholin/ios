c206da28e075a292681aaee7abc90ea7fab08535
MikalaiKholin <kholin.mikalai@gmail.com>
2019-03-31 08:04:46 -0700

    t2_1

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
@@ -1,5 +1,22 @@
 #import "KidnapperNote.h"
 
 @implementation KidnapperNote
-// your code here
+
+- (BOOL)checkMagazine:(NSString *)magaine note:(NSString *)note;{
+    
+    NSString *magazineInLowercase = [magaine lowercaseString];
+    NSArray *wordsInNote = [note componentsSeparatedByString:@" "];
+    
+    for (NSString *wordInNote in wordsInNote){
+        NSString *wordInNoteInLowercase = [wordInNote lowercaseString];
+        if (![magazineInLowercase containsString:wordInNoteInLowercase]){
+            return NO;
+        }
+        magazineInLowercase = [magazineInLowercase stringByReplacingOccurrencesOfString:wordInNoteInLowercase withString:@""];
+        
+    }
+    
+    return YES;
+}
+
 @end
