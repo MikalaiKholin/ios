bd9b9cbd5457c01ceecd7ad8ca934e11a643735c
Mikalai Kholin <kholin.mikalai@gmail.com>
2019-04-21 15:07:27 +0300

    4

---
+++ Tasks/Task4/RSSchool_T4/RSSchool_T4/PhoneNumberViewController.h
@@ -0,0 +1,17 @@
+//
+//  PhoneNumberViewController.h
+//  RSSchool_T4
+//
+//  Created by Mikalai Kholin on 21/04/2019.
+//  Copyright © 2019 iOSLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhoneNumberViewController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tasks/Task4/RSSchool_T4/RSSchool_T4/PhoneNumberViewController.m
@@ -0,0 +1,13 @@
+//
+//  PhoneNumberViewController.m
+//  RSSchool_T4
+//
+//  Created by Mikalai Kholin on 21/04/2019.
+//  Copyright © 2019 iOSLab. All rights reserved.
+//
+
+#import "PhoneNumberViewController.h"
+
+@implementation PhoneNumberViewController
+
+@end
