2e81965f6097eea48d31f6c3b8365566c82a0e59
Mikalai Kholin <kholin.mikalai@gmail.com>
2019-04-22 10:54:56 +0300

    4_2

--- Tasks/Task4/RSSchool_T4/RSSchool_T4/PhoneNumberViewController.m
+++ Tasks/Task4/RSSchool_T4/RSSchool_T4/PhoneNumberViewController.m
@@ -13,7 +13,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+
     
     
     UITextField *phoneNumber =[[UITextField alloc] initWithFrame:CGRectMake(5, 300, 300, 20)];
@@ -39,9 +39,9 @@
     [phoneNumber replaceCharactersInRange:NSMakeRange(0, [self checkPlusPosition:phoneNumber]) withString:@""];
     [phoneNumber replaceCharactersInRange:NSMakeRange(0, [self checkCountryFlagInPosition:phoneNumber]) withString:@""];
     
-    NSRegularExpression *rx = [NSRegularExpression regularExpressionWithPattern:@"[^0-9+]" options:0 error:nil];
+    NSRegularExpression *rx = [NSRegularExpression regularExpressionWithPattern:@"[^0-9]" options:0 error:nil];
     [rx replaceMatchesInString:phoneNumber options:0 range:NSMakeRange(0, [phoneNumber length]) withTemplate:@""];
-    phoneNumber = [phoneNumber stringByReplacingOccurrencesOfString:@"+" withString:@""];
+    //phoneNumber = [phoneNumber stringByReplacingOccurrencesOfString:@"+" withString:@""];
     
     phoneNumber = [self textFromNumber:phoneNumber];
     
@@ -61,9 +61,6 @@
     for (Country *country in countries){
         if (country.code.length <= number.length){
             if ([country.code isEqualToString:[number substringToIndex:(country.code.length)]]){
-                [phoneNumberWithFormat appendFormat:(@"%@", country.flag)];
-                [phoneNumberWithFormat appendFormat:@"+"];
-                [phoneNumberWithFormat appendFormat:(@"%@ ", country.code)];
                 [phoneNumberWithFormat appendFormat:[self phoneNumberFormat:[number substringFromIndex:country.code.length] :country]];
                 return phoneNumberWithFormat;
             }
@@ -73,7 +70,6 @@
     [countries release];
     
     [phoneNumberWithFormat appendFormat:@"+"];
-    
     if (number.length>=12){
         [phoneNumberWithFormat appendFormat:(@"%@", [number substringToIndex:12])];
         return phoneNumberWithFormat;
@@ -82,44 +78,57 @@
     return phoneNumberWithFormat;
 }
 
--(NSMutableString*) phoneNumberFormat: (NSString*)number :(Country*) country{
+-(NSString*) phoneNumberFormat: (NSString*)number :(Country*) country{
+    NSMutableString *phoneNumber = [[NSMutableString alloc] initWithString:@""];
+    //[phoneNumber appendFormat:(@"%@", country.flag)];
+    //[phoneNumber appendFormat:@"+"];
+    //[phoneNumber appendFormat:(@"%@ ", country.code)];
     
-    NSLog(@"%@", country.name);
+    NSLog(@"number -%@-", number);
     NSString *mask = [NSString new];
     
+    
     if (country.numberOfDigitsInPhoneNumber == 8){
+        [phoneNumber appendFormat:@"%@+%@", country.flag, country.code];
         mask = @"(xx) xxx-xxx";
     }
-    if (country.numberOfDigitsInPhoneNumber == 9){
+    if ((country.numberOfDigitsInPhoneNumber == 9)&!([country.shortName isEqualToString:@"KZ"])){
+        [phoneNumber appendFormat:@"%@+%@", country.flag, country.code];
         mask = @"(xx) xxx-xx-xx";
     }
+    if ([country.shortName isEqualToString:@"KZ"]){
+        [phoneNumber appendFormat:@"%@+%@", country.flag, @"7(7"];
+        mask = @"xx) xxx-xx-xx";
+    }
     if (country.numberOfDigitsInPhoneNumber == 10){
+        [phoneNumber appendFormat:@"%@+%@", country.flag, country.code];
         mask = @"(xxx) xxx-xx-xx";
     }
     
-    NSMutableString *phoneNumber = [[NSMutableString alloc] initWithString:@""];
-    NSLog(@"%@", phoneNumber);
+    
+    NSLog(@"%@", mask);
+    NSLog(@"pn --%@---", phoneNumber);
     
     int j=0;
     for (int i=0; i<number.length; i++){
         if (i>=country.numberOfDigitsInPhoneNumber){
-            return phoneNumber;
+            break;
         }
         NSLog(@"%@", phoneNumber);
         while (!([[NSString stringWithFormat:@"%c", [mask characterAtIndex:j]] isEqualToString:@"x"])&(j<mask.length-1)){
             [phoneNumber appendFormat:[NSString stringWithFormat:@"%c", [mask characterAtIndex:j]]];
             j++;
             NSLog(@"%d", j);
-            //cm = [NSString stringWithFormat:@"%c", [mask characterAtIndex:j]];
         }
         NSLog(@"%@", phoneNumber);
         [phoneNumber appendFormat:[NSString stringWithFormat:@"%c", [number characterAtIndex:i]]];
         NSLog(@"%@", phoneNumber);
         j++;
     }
-    
+    NSString *result =[[[NSString alloc] initWithString:phoneNumber]autorelease];
+    [phoneNumber release];
     [mask release];
-    return phoneNumber;
+    return result;
 }
 
 
@@ -151,18 +160,18 @@
 }
 
 -(NSArray*) countriesList{
-    NSArray *countries = [[[NSArray alloc] initWithObjects:[[Country alloc]initWithName:@"Russia" andShortName:@"RU" andCode:@"7" andFlag:@"🇷🇺" andNumberOfDigitsInPhoneNumber:10],
-                                                          //[[Country alloc]initWithName:@"Kazakhstan" andShortName:@"KZ" andCode:@"7" andFlag:@"🇰🇿" andNumberOfDigitsInPhoneNumber:10],
-                                                          [[Country alloc]initWithName:@"Moldova" andShortName:@"MD" andCode:@"373" andFlag:@"🇲🇩" andNumberOfDigitsInPhoneNumber:8],
-                                                          [[Country alloc]initWithName:@"Armenia" andShortName:@"AM" andCode:@"374" andFlag:@"🇦🇲" andNumberOfDigitsInPhoneNumber:8],
-                                                          [[Country alloc]initWithName:@"Belarus" andShortName:@"BY" andCode:@"375" andFlag:@"🇧🇾" andNumberOfDigitsInPhoneNumber:9],
-                                                          [[Country alloc]initWithName:@"Ukraine" andShortName:@"UA" andCode:@"380" andFlag:@"🇺🇦" andNumberOfDigitsInPhoneNumber:9],
-                                                          [[Country alloc]initWithName:@"Tajikistan" andShortName:@"TJ" andCode:@"992" andFlag:@"🇹🇯" andNumberOfDigitsInPhoneNumber:9],
-                                                          [[Country alloc]initWithName:@"Turkmenistan" andShortName:@"TM" andCode:@"993" andFlag:@"🇹🇲" andNumberOfDigitsInPhoneNumber:8],
-                                                          [[Country alloc]initWithName:@"Azerbaijan" andShortName:@"AZ" andCode:@"994" andFlag:@"🇦🇿" andNumberOfDigitsInPhoneNumber:9],
-                                                          [[Country alloc]initWithName:@"Kyrgyzstan" andShortName:@"KG" andCode:@"996" andFlag:@"🇰🇬" andNumberOfDigitsInPhoneNumber:9],
-                                                          [[Country alloc]initWithName:@"Uzbekistan" andShortName:@"UZ" andCode:@"998" andFlag:@"🇺🇿" andNumberOfDigitsInPhoneNumber:9]
-                          , nil] autorelease];
+    NSArray *countries = [[[NSArray alloc] initWithObjects:[[Country alloc]initWithName:@"Kazakhstan" andShortName:@"KZ" andCode:@"77" andFlag:@"🇰🇿" andNumberOfDigitsInPhoneNumber:9],
+                           [[Country alloc]initWithName:@"Russia" andShortName:@"RU" andCode:@"7" andFlag:@"🇷🇺" andNumberOfDigitsInPhoneNumber:10],
+                           [[Country alloc]initWithName:@"Moldova" andShortName:@"MD" andCode:@"373" andFlag:@"🇲🇩" andNumberOfDigitsInPhoneNumber:8],
+                           [[Country alloc]initWithName:@"Armenia" andShortName:@"AM" andCode:@"374" andFlag:@"🇦🇲" andNumberOfDigitsInPhoneNumber:8],
+                           [[Country alloc]initWithName:@"Belarus" andShortName:@"BY" andCode:@"375" andFlag:@"🇧🇾" andNumberOfDigitsInPhoneNumber:9],
+                           [[Country alloc]initWithName:@"Ukraine" andShortName:@"UA" andCode:@"380" andFlag:@"🇺🇦" andNumberOfDigitsInPhoneNumber:9],
+                           [[Country alloc]initWithName:@"Tajikistan" andShortName:@"TJ" andCode:@"992" andFlag:@"🇹🇯" andNumberOfDigitsInPhoneNumber:9],
+                           [[Country alloc]initWithName:@"Turkmenistan" andShortName:@"TM" andCode:@"993" andFlag:@"🇹🇲" andNumberOfDigitsInPhoneNumber:8],
+                           [[Country alloc]initWithName:@"Azerbaijan" andShortName:@"AZ" andCode:@"994" andFlag:@"🇦🇿" andNumberOfDigitsInPhoneNumber:9],
+                           [[Country alloc]initWithName:@"Kyrgyzstan" andShortName:@"KG" andCode:@"996" andFlag:@"🇰🇬" andNumberOfDigitsInPhoneNumber:9],
+                           [[Country alloc]initWithName:@"Uzbekistan" andShortName:@"UZ" andCode:@"998" andFlag:@"🇺🇿" andNumberOfDigitsInPhoneNumber:9]
+                           , nil] autorelease];
     return countries;
     
 }
